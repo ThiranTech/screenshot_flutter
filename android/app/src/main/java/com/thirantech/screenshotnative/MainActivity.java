a9406e48bc65b76e1bf55c15f521bfe3a2da1e2d
Prabhu K <prabhuk@thirantech.com>
2018-08-02 19:16:11 +0530

    CleanUp

--- android/app/src/main/java/com/thirantech/screenshotnative/MainActivity.java
+++ android/app/src/main/java/com/thirantech/screenshotnative/MainActivity.java
@@ -1,21 +1,12 @@
 package com.thirantech.screenshotnative;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.StrictMode;
-import android.view.View;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.text.DateFormat;
-import java.util.Date;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.plugin.common.MethodCall;
@@ -23,7 +14,6 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-import static java.lang.Thread.sleep;
 
 public class MainActivity extends FlutterActivity  {
   private static final String CHANNEL = "team.native.io/screenshot";
@@ -44,8 +34,6 @@ public class MainActivity extends FlutterActivity  {
               public void onMethodCall(MethodCall call, Result result) {
                 if (call.method.equals("takeScreenshot")) {
                     String fileUri =call.argument("filePath").toString();
-                    Uri filePath=Uri.parse(fileUri);
-                    //takeScreenshot();
                     sendMail(Uri.fromFile(new File(fileUri)));
                     result.success("Done");
                 }else {
@@ -63,50 +51,6 @@ public class MainActivity extends FlutterActivity  {
     //Uri uri =Uri.parse(filePath);
     intent.putExtra(Intent.EXTRA_STREAM, filePath);
     startActivity(Intent.createChooser(intent, "Send email..."));
-  }
-  @TargetApi(Build.VERSION_CODES.CUPCAKE)
-  private void takeScreenshot() {
-    Date now = new Date();
-    android.text.format.DateFormat.format("yyyy-MM-dd", now);
-
-    try {
-      // image naming and path  to include sd card  appending name you choose for file
-      String mPath = Environment.getExternalStorageDirectory().toString() + "/" + "Screeshot" + ".jpg";
-
-      // create bitmap screen capture
-      View v1 = getWindow().getDecorView().getRootView();
-      v1.setDrawingCacheEnabled(true);
-      v1.buildDrawingCache();
-      sleep(5000);
-      Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
-      Rect frame = new Rect();
-      getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
-      int statusBarHeight = frame.top;
-
-      //Find the screen dimensions to create bitmap in the same size.
-      int width = this.getWindowManager().getDefaultDisplay().getWidth();
-      int height = this.getWindowManager().getDefaultDisplay().getHeight();
-
-      //Bitmap b = Bitmap.createBitmap(bitmap, 0, 0, width, height);
-
-      File imageFile = new File(
-              android.os.Environment.getExternalStorageDirectory(),"teamfiles");
-      if(!imageFile.exists()) {
-        imageFile.mkdirs();
-      }
-
-      String path =imageFile + "/screenshot.png";
-
-      FileOutputStream outputStream = new FileOutputStream(path);
-      int quality = 90;
-      bitmap.compress(Bitmap.CompressFormat.PNG, quality, outputStream);
-      outputStream.flush();
-      outputStream.close();
-      //openScreenshot(imageFile);
-    } catch (Throwable e) {
-      // Several error may come out with file handling or DOM
-      e.printStackTrace();
-    }
   }
 
 }
